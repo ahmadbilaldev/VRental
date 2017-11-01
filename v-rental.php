46a0ebeb9b69a6b25d160e4b1e5ce5605c5337cd
Ahmad Bilal <atbilal@gmail.com>
2017-11-01 19:13:46 +0500

    Add files

---
+++ v-rental.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: V-Rental
+ * Plugin URI: http://github.com/mrahmadbilal/VRental
+ * Description: A plugin rental bookings.
+ * Author: mrahmadbilal
+ * Author URI: http://github.com/mrahmadbilal
+ * Text Domain: VR
+ * Version: 1.0.0
+ * License: GPL v2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package VR
+ *
+ * GitHub Plugin URI: https://github.com/mrahmadbilal/VRental
+ * GitHub Branch: master
+ */
+
+// If this file is called directly, abort.
+If ( ! defined( 'WPINC' ) ) {
+	die;
+}
