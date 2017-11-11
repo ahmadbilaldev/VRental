1293d45bc16bee630f4a073ded089c71fe8891d4
Ahmad Bilal <atbilal@gmail.com>
2017-11-11 20:46:02 +0500

    Add metabox

--- assets/metaboxes/vr-metabox.php
+++ assets/metaboxes/vr-metabox.php
@@ -1,15 +1,21 @@
 <?php
 
+/**
+ * The metabox file.
+ *
+ * @since 1.0.0
+ */
 function vr_metabox_cb()
 {
     ?>
-    <label for="my_meta_box_text">Text Label</label>
-    <input type="text" name="my_meta_box_text" id="my_meta_box_text" />
+    	<label for="my_meta_box_text">Text Label</label>
+    	<input type="text" name="my_meta_box_text" id="my_meta_box_text" />
     <?php
 }
 
 add_action( 'add_meta_boxes', 'vr_metabox' );
+
 function vr_metabox()
 {
-    add_meta_box( 'my-meta-box-id', 'My First Meta Box', 'vr_metabox_cb', 'post', 'normal', 'high' );
+    add_meta_box( 'my-meta-box-id', 'Meta Box', 'vr_metabox_cb', 'post', 'side', 'high' );
 }
